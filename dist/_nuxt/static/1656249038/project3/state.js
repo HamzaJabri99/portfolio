34f4f60e4d6be62bbf4f7590eaf1e8608e5a5527
unknown <jabri.oldd@gmail.com>
2022-06-26 14:54:40 +0100

    deploy files

--- dist/_nuxt/static/1656249038/project3/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1656249038",layout:"default",error:a,serverRendered:true,routePath:"\u002Fproject3",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1656251662/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FContact","\u002FHome","\u002FAbout","\u002FThanks","\u002FProjects","\u002FRick & Morty Wiki","\u002FFoodies","\u002Fproject3"]})
