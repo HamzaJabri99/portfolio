495e8d26c483dcbb79320785682d66964c8d5cae
unknown <jabri.oldd@gmail.com>
2022-06-26 13:24:03 +0100

    deploy files

--- dist/_nuxt/static/1656245703/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FThanks","\u002FContact","\u002FHome","\u002FProjects","\u002FAbout","\u002FRick & Morty Wiki","\u002FFoodies","\u002Fproject3"]})
